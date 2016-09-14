4f5ac19ebf56cd1776b80aceb96bf4e716c5bec4
PeterQStarlord <kibblesamgreat@gmail.com>
2016-09-13 20:09:29 -0700

    Level loader
    allows the player to load another level but pressing E on the object the script is attatched to

---
+++ LevelLoader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    private bool playerInZone;
+
+    public string levelToLoad;
+
+    void Start()
+    {
+        playerInZone = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInZone)
+        {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            playerInZone = true;
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            playerInZone = false;
+        }
+
+    }
+}
