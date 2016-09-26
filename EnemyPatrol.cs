87c2cc098333e1623fc58749b45fa2040dab4ff1
jlichtenfeld3 <jlichtenfeld3@gmail.com>
2016-09-26 14:05:43 -0700

    Add files via upload

---
+++ EnemyPatrol.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrol  : MonoBehaviour  { 
+
+	public float moveSpeed;
+	public bool moveRight;
+	public Transform wallCheck;
+	public float wallCheckRadius; 
+	public LayerMask whatIsWall; 
+	private bool hittingWall; 
+
+	private bool notAtEdge;
+	public Transform edgeCheck; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{ hittingWall = Physics2D.OverlapCircle (wallCheck.position, wallCheckRadius, whatIsWall);
+		notAtEdge = Physics2D.OverlapCircle (edgeCheck.position, wallCheckRadius, whatIsWall);
+
+
+		if (hittingWall || !notAtEdge)
+			moveRight = !moveRight;
+
+
+		if (moveRight) {
+			transform.localScale = new Vector3 (-1f, 1f, 1f);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		} 
+			 else {
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+	}
+}
