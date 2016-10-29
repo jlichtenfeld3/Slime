0bac07374e6596f3314450735c172478427f2c94
dlahm17 <dlahm17@yahoo.com>
2016-10-29 15:22:14 -0700

    This is the main camera prefab
    apply the main camera script when you can.
    Have the "target" be your main character
    and have the smoothing equal 5

---
+++ cameraFollow2DPlatformer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow2DPlatformer : MonoBehaviour {
+	public Transform target; //what the camera is following
+	public float smoothing; //dampening effect that occurs on the camera
+
+
+	Vector3 offset; //difference between character and camera
+	float lowY;  //lowest point the camera can go
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+
+		lowY = transform.position.y;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 targetCamPos = target.position + offset;
+
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+		if (transform.position.y < lowY)
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+
+	
+	}
+}
